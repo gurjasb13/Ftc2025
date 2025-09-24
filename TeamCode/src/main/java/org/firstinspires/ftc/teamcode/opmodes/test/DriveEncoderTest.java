90b1f87a69b8369fc37984a78cec461790e84834
gurjasb13 <159503555+gurjasb13@users.noreply.github.com>
2025-09-24 08:26:03 -0400

    reinstalled sdk
    reinstalled roadrunner, made drivebase encoder test code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/DriveEncoderTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.opmodes.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class DriveEncoderTest extends OpMode {
+    private DcMotor par1;
+    private DcMotor par2;
+    private DcMotor perp;
+
+    @Override
+    public void init() {
+        par1 = hardwareMap.get(DcMotor.class, "par1");
+        par2 = hardwareMap.get(DcMotor.class, "par2");
+        perp = hardwareMap.get(DcMotor.class, "perp");
+
+        telemetry.addData("Status", "Initialized");
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("par1", par1.getCurrentPosition());
+        telemetry.addData("par2", par2.getCurrentPosition());
+        telemetry.addData("perp", perp.getCurrentPosition());
+        telemetry.update();
+    }
+}
